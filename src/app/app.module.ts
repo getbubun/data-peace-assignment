c9a6935dc21ac425c5b5a95b1308bb79ef58ff41
Manab <getbubun@gmail.com>
2020-02-15 01:02:32 +0530

    almost !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DetailedViewComponent } from './detailed-view/detailed-view.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { DetailedViewComponent } from "./detailed-view/detailed-view.component";
+
+import { ApiService } from "./api.service";
+import { SearchPipe } from "./search.pipe";
+// import { HighlightPipe } from "./highlight.pipe";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      HomeComponent,
-      DetailedViewComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      HttpClientModule
-      
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DetailedViewComponent,
+    SearchPipe
+    // HighlightPipe
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../api.service";
+import { Router } from "@angular/router";
+import { SearchPipe } from "../search.pipe";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  userList:any;
-  constructor(private apiService: ApiService, private router: Router) { }
+  userList: any;
+  myFilter: any;
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit() {
     this.userData();
   }
 
-  userData(){
-    this.apiService.getData().subscribe(data =>{
+  userData() {
+    this.apiService.getData().subscribe(data => {
       this.userList = data;
     }),
-    error=>{
-      console.error("Some error occured !")
-    }
+      error => {
+        console.error("Some error occured !");
+      };
   }
 
-  viewUser(user){
+  viewUser(user) {
     this.apiService.setuser = user;
-    this.router.navigate(['/user/'+ user.id]);
+    this.router.navigate(["/user/" + user.id]);
   }
-
 }
